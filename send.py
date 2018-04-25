72f4c52d032bcaa2879884142ebdb04d7c8d4fc3
sanip <sanip@localhost.localdomain>
2018-04-25 23:24:34 +0545

    added comments

--- send.py
+++ send.py
@@ -7,16 +7,19 @@ fromaddr = 'saneprijal@gmail.com'
 toaddr = 'sharmagiriaayush@gmail.com'
 subject = 'Dun!Dun!Dun!'
 body = "Maa ka bhosda madarchod"
+#prepare the message to be sent
 msg = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (fromaddr, ", ".join(toaddr), subject, body)
+
+#gmail username and password for authentication
 username = 'saneprijal@gmail.com'
-password = '9843440022'
+password = ''   #your password
 try:
-    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server = smtplib.SMTP("smtp.gmail.com", 587)    #create a smtp connection object
     server.ehlo()
-    server.starttls()
+    server.starttls()   #secure smtp using tls
     server.ehlo()
-    server.login(username, password)
-    server.sendmail(fromaddr, toaddr, msg)
+    server.login(username, password)    #login to your gmail
+    server.sendmail(fromaddr, toaddr, msg)  #send the mail to the receipent
     server.quit()
     print("success")
 except smtplib.SMTPAuthenticationError:
